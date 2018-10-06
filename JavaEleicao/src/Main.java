a98aaa237e13041a64c388ffb52b832bb3b6e509
1ldomal2 <lucasmfccom@gmail.com>
2018-10-06 04:22:28 -0300

    ok

--- JavaEleicao/src/Main.java
+++ JavaEleicao/src/Main.java
@@ -8,19 +8,12 @@ import Modelo.Partido;
 public class Main {
 
 	public static void main(String[] args) {
-		/*CandidatoDAO DAO =new CandidatoDAO();
-		DAO.CriarCandidato("Nome", "Numero", "06680923603", "NomePartido", "123");
-		DAO.CriarCandidato("Nome", "Numero", "06680923603", "NomePartido", "123");
-		DAO.CriarCandidato("Nome", "Numero", "06680923603", "NomePartido", "123");
+		CandidatoDAO DAO =new CandidatoDAO();
+		DAO.CriarCandidato("Nome", "123", "06680923603", "123", "qqq");
+		DAO.CriarCandidato("Nome", "321", "06680923603", "123", "aa");
+		DAO.CriarCandidato("Nome", "123", "06680923603", "123", "ss");
 		String str=DAO.makeJson();
-		System.out.println(str);*/
-
-		Documentos doc =new Documentos();
-		
-		System.out.println(doc.validaCpf("759.613.040-27"));
-		System.out.println(doc.validaCpf("12341233123"));
-		System.out.println(doc.validaCpf("01232701033"));
-		System.out.println(doc.validaCpf("23519440032"));
+		System.out.println(str);
 		
 	}
 
