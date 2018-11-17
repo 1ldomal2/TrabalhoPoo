f64a10004fae094a0be213246fabf2c525c9a9f3
1ldomal2 <lucasmfpiu@gmail.com>
2018-11-17 19:38:38 -0200

    ko

--- Dependencias/Projeto/src/main/java/MODELO/Deputado.java
+++ Dependencias/Projeto/src/main/java/MODELO/Deputado.java
@@ -11,7 +11,25 @@ package MODELO;
  */
 public class Deputado  extends Candidato{
     private String estado = null;
-    
+
+    public Deputado(String Nome, String Numero, String Cpf, String NumeroPartido, String NomePartido,String Sigla) throws Throwable{
+        //Chama o construtor do pai
+        super(Nome,Numero,Cpf,NumeroPartido,NomePartido);
+        //Se o Cpf foi valido continua fazendo o Deputado
+        if(this.getNome()!= null){
+            //Seta o Estado
+            estado=Estados.valueOf(Sigla).getSigla();
+            //Se estado=null nao foi possivel criar o Deputado
+            if(estado == null){
+                this.finalize();
+            }else{
+               if(estado == Estados.BR.getSigla()){//Se vier BR é presidente e não deputado Federal
+                this.finalize();
+               } 
+            }
+        }
+        
+    }
     public void setEstado(String estado){
         this.estado=estado;
         return;
--- Dependencias/Projeto/src/main/java/MODELO/Presidente.java
+++ Dependencias/Projeto/src/main/java/MODELO/Presidente.java
@@ -10,5 +10,29 @@ package MODELO;
  * @author lucas
  */
 public class Presidente extends Candidato {
-    
+
+   
+    public Presidente(String Nome, String Numero, String Cpf, String NumeroPartido, String NomePartido,String Sigla) throws Throwable{
+        //Chama o construtor do pai
+        super(Nome,Numero,Cpf,NumeroPartido,NomePartido);
+        //Se o Cpf foi valido continua fazendo o Deputado
+        if(this.getNome()!= null){
+         
+            if(Sigla != Estados.BR.getSigla()){
+                this.finalize();
+            }
+        }
+        
+    }
+    public Presidente(String Nome, String Numero, String Cpf, String NumeroPartido, String NomePartido,int Cod) throws Throwable{
+        //Chama o construtor do pai
+        super(Nome,Numero,Cpf,NumeroPartido,NomePartido);
+        //Se o Cpf foi valido continua fazendo o Deputado
+        if(this.getNome()!= null){
+            if(Cod != Estados.BR.getCod()){
+                this.finalize();
+            }
+        }
+        
+    }
 }
